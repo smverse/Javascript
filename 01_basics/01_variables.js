ebf978bbb04fb8ea8bcbbdadf7ef2ddf1a691406
shubham0323 <123639780+shubham0323@users.noreply.github.com>
2026-05-15 16:20:15 +0530

    Add initial variable declarations

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144223
+let accountEmail = "smverse@gmail.com"
+var accountPassword = "12345678"
+accountCity = "Jaipur"
+
+
+//accountId = 2 // not allowed
+
+
+accountEmail= "hc@email.com"
+accountPassword = "9876543211"
+accountCity = "Lucknow"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
