e60dfd74dceec12d08aa1f65f6bc06e8118aea53
shubham0323 <123639780+shubham0323@users.noreply.github.com>
2026-05-15 16:51:50 +0530

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+let name = "Shubham"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53 something
+// bigint
+// string => ""
+// boolean => true/false
+// null=> standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof " shubham ") // string
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
