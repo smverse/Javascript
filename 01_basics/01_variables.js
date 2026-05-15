f8247ce419aac40e15924830551e2f863679f309
shubham0323 <123639780+shubham0323@users.noreply.github.com>
2026-05-15 16:28:52 +0530

    Updated file

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId = 144223
 let accountEmail = "smverse@gmail.com"
 var accountPassword = "12345678"
 accountCity = "Jaipur"
+let accountState;
 
 
 //accountId = 2 // not allowed
@@ -11,6 +12,13 @@ accountEmail= "hc@email.com"
 accountPassword = "9876543211"
 accountCity = "Lucknow"
 
+
 console.log(accountId);
 
-console.table([accountId, accountEmail, accountPassword, accountCity])
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
